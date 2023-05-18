db5cb5aea2a0a615391b405966d88c6f6420425f
Mariyam Rahman <mariyamrahman38151@gmail.com>
2023-05-18 23:03:33 +0600

    login and register pages are added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
             </p>
             <a
               href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mr-4"
+              className="inline-flex  justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mr-4"
             >
               Get started
               <svg
