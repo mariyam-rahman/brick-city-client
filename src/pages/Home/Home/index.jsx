2c91f58bc165cafb53739cd4e15aa3d5d274cf4b
Mariyam Rahman <mariyamrahman38151@gmail.com>
2023-05-18 17:15:16 +0600

    review  section added in homepage

--- src/pages/Home/Home/index.jsx
+++ src/pages/Home/Home/index.jsx
@@ -2,6 +2,7 @@ import Gallery from "../Gallery/Gallery";
 import ShopByCategory from "../ShopByCategory/ShopByCategory";
 import Banner from "../Banner/Banner";
 import About from "../About/About";
+import Review from "../Review/Review";
 
 const index = () => {
   return (
@@ -9,6 +10,7 @@ const index = () => {
       <Banner></Banner>
       <About></About>
       <ShopByCategory></ShopByCategory>
+      <Review></Review>
       <Gallery></Gallery>
     </div>
   );
