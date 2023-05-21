18224d088a9863ed10ef8114046b5b6e751c9e0b
Mariyam Rahman <mariyamrahman38151@gmail.com>
2023-05-21 23:32:33 +0600

    all issues fixed

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,15 +3,15 @@ const Banner = () => {
   return (
     <div>
       <section
-        style={{ backgroundSize: "cover" }}
-        className="bg-white dark:bg-gray-900 bg-[url('//assets/hero.png')] "
+        style={{ backgroundSize: "cover", height: "80vh" }}
+        className="bg-white dark:bg-gray-900 bg-[url('/assets/hero.png')] "
       >
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12  ">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
               Building Dreams, One Brick at a Time!
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <p className="max-w-2xl mb-6 font-light text-gray-900 lg:mb-8 md:text-lg lg:text-xl light:text-gray-400 font-medium">
               Explore endless brick-building adventures at our Brick City.
               Discover joy, creativity, and imagination in every block
             </p>
